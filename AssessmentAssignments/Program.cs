1d9378b32b03bb77523ba18742939a1874139503
Hamza Afridi <hamzaa.wp65@gmail.com>
2023-11-21 12:48:32 +0500

    Add project files.

---
+++ AssessmentAssignments/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace AssessmentAssignments
+{
+    class Program
+    {
+
+        //1ST PROGRAM
+
+        public static void NumberofSocks()
+        {
+            Console.WriteLine("Enter Letters of the Socks: ");
+            string sockLetters = Console.ReadLine();
+
+            int sockPairs = 0;
+
+            int[] letterCount = new int[128];
+
+            foreach (char sock in sockLetters)
+            {
+                letterCount[sock]++;
+            }
+
+            foreach (int count in letterCount)
+            {
+                sockPairs = sockPairs + count / 2;
+            }
+
+            Console.WriteLine($"The Number of Pairs are {sockPairs}");
+        }
+
+
+
+
+        //2ND PROGRAM
+
+        public static void DisplaySeriesAndSum(int numberOfTerms)
+        {
+            int sum = 0;
+            int Digit = 9;
+
+            Console.Write("The series: ");
+
+            for (int i = 0; i < numberOfTerms; i++)
+            {
+                Console.Write($"{Digit} ");
+
+                sum = sum + Digit;
+
+                Digit = Digit * 10 + 9;
+            }
+
+            Console.WriteLine($"\nThe sum of the series = {sum}");
+        }
+
+
+
+        
+
+        //3RD PROGRAM
+
+        static void AddTwoIndirectly(string num1, string num2)
+        {
+            int maxLength;
+
+            if (num1.Length > num2.Length)
+            {
+                maxLength = num1.Length;
+            }
+            else
+            {
+                maxLength = num2.Length;
+            }
+
+            while (num1.Length < maxLength)
+            {
+                num1 = '0' + num1;
+            }
+
+            while (num2.Length < maxLength)
+            {
+                num2 = '0' + num2;
+            }
+
+            string result = "";
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                int digit1 = int.Parse(num1[i].ToString());
+                int digit2 = int.Parse(num2[i].ToString());
+
+                result += digit1 + digit2;
+            }
+
+            Console.WriteLine("Concatenated Result: " + result);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            //1ST PROGRAM
+            //NumberofSocks();
+
+
+
+            //// 2nd PROGRAM
+            //Console.WriteLine("Input the number of terms: ");
+            //int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+            //DisplaySeriesAndSum(numberOfTerms);
+
+           
+
+
+            //3RD PROGRAM
+            Console.Write("Enter the first number: ");
+            string num1 = Console.ReadLine();
+            Console.Write("Enter the second number: ");
+            string num2 = Console.ReadLine();
+            AddTwoIndirectly(num1, num2);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AssessmentAssignments/Refernce.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentAssignments
+{
+    class Refernce
+    {
+        public static void VowelsConsonent()
+        {
+            int Vowels = 0;
+            int Consonent = 0;
+
+            Console.WriteLine("Enter a Sentence: ");
+            string sentence = Console.ReadLine();
+
+            for (int i = 1; i < sentence.Length; i++)
+
+                if (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+                {
+                    Vowels++;
+                }
+
+                else
+                {
+                    Consonent++;
+                }
+
+            Console.WriteLine($"The Total number of Vowels is: {Vowels}");
+            Console.WriteLine($"The Total number of Vowels is: {Consonent}");
+
+        }
+    }
+}
