465193a282d564fd561b9f2233f9cb8bf985beb2
Hamza Afridi <hamzaa.wp65@gmail.com>
2023-11-21 16:39:38 +0500

    5 Questions Done

--- AssessmentAssignments/Program.cs
+++ AssessmentAssignments/Program.cs
@@ -97,6 +97,79 @@ namespace AssessmentAssignments
 
 
 
+        //4th PROGRAM
+        static void CountPositivesAndSumNegatives()
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter element {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int countPositives = 0;
+            int sumNegatives = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    countPositives++;
+                }
+                else if (number < 0)
+                {
+                    sumNegatives += number;
+                }
+            }
+
+            // Display the entered array
+            Console.WriteLine("Entered array:");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            // Display the result
+            Console.WriteLine($"\nResult: [{countPositives}, {sumNegatives}]");
+        }
+
+
+
+
+
+        //5th Program
+        static void BubbleSortAndDisplay(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    // Swap if the element found is greater than the next element
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Display the sorted array
+            Console.WriteLine("\nSorted array:");
+            foreach (int number in arr)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
 
@@ -110,16 +183,34 @@ namespace AssessmentAssignments
             //int numberOfTerms = Convert.ToInt32(Console.ReadLine());
             //DisplaySeriesAndSum(numberOfTerms);
 
-           
 
 
-            //3RD PROGRAM
-            Console.Write("Enter the first number: ");
-            string num1 = Console.ReadLine();
-            Console.Write("Enter the second number: ");
-            string num2 = Console.ReadLine();
-            AddTwoIndirectly(num1, num2);
-            
+
+            ////3RD PROGRAM
+            //Console.Write("Enter the first number: ");
+            //string num1 = Console.ReadLine();
+            //Console.Write("Enter the second number: ");
+            //string num2 = Console.ReadLine();
+            //AddTwoIndirectly(num1, num2);
+
+
+
+            ////4th PROGRAM
+            //CountPositivesAndSumNegatives();
+            // CountPosSumNeg();
+
+
+
+
+            //5th Program
+            int[] numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter integer {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            BubbleSortAndDisplay(numbers);
+
 
 
             Console.ReadLine();
